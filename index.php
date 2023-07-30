2b6cb5f98c0a0d30687620a027345a518f2deee2
yathavanyogarajah <yathavan@surge.global>
2023-07-30 14:22:08 +0530

    new base theme done

--- index.php
+++ index.php
@@ -1,31 +1,32 @@
 <?php get_header(); ?>
 
-<div class="container mx-auto my-8">
+    <div class="container max-w-screen-xl mx-auto my-8 flex">
 
-    <!-- This example requires Tailwind CSS v2.0+ -->
-    <div class="relative pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
-        <div class="relative max-w-7xl mx-auto">
-            <div class="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 xl:grid-cols-4 lg:max-w-none">
+        <!-- This example requires Tailwind CSS v2.0+ -->
+        <div class="w-2/3 px-4">
+            <?php if (have_posts()) : ?>
+                <?php
+                while (have_posts()) :
+                    the_post();
+                    ?>
 
+                    <?php get_template_part('template-parts/content', get_post_format()); ?>
 
-                <?php if ( have_posts() ) : ?>
-                    <?php
-                    while ( have_posts() ) :
-                        the_post();
-                        ?>
+                <?php endwhile; ?>
 
-                        <?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+            <?php endif; ?>
+        </div>
 
-                    <?php endwhile; ?>
-
-                <?php endif; ?>
-
+        <div class="w-1/3 px-4">
+            <div class="bg-white rounded-2xl p-6">
+                <h3>
+                    Featured posts
+                </h3>
             </div>
         </div>
+
+
     </div>
-
-
-</div>
 
 <?php
 get_footer();
--- template-parts/content.php
+++ template-parts/content.php
@@ -10,28 +10,27 @@ if (empty($the_cat)) {
     $the_cat = [];
 }
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class('flex flex-col rounded-lg shadow-lg overflow-hidden'); ?> >
-    <img class="h-48 w-full object-cover" src="<?php echo $isofeaturedimage; ?>" alt="">
 
-    <div class="flex-1 bg-white p-4 pt-3 flex flex-col justify-between">
-        <div class="flex-1">
-            <a href="#" class="block mt-2">
-                <?php the_title(sprintf('<h2 class="text-xl md:text-2xl font-bold leading-none mb-1"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?>
-
-                <p class="text-sm font-medium text-indigo-600">
-                    <a href="#"
-                       class="hover:underline"> <?php echo implode(',', array_column($the_cat, 'cat_name')); ?> </a>
-
-                </p>
-                <p class="mt-3 text-base text-gray-500 line-clamp-6">
-                    <?php
-                    echo wp_strip_all_tags( wp_trim_words( get_the_content(), 40, '...' ) );
-                    ?>
-                </p>
-            </a>
+    <article id="post-<?php the_ID(); ?>" <?php post_class('flex flex-row bg-white p-6 rounded-2xl mb-9'); ?>>
+        <img class="w-2/6 rounded-2xl object-cover" src="<?php echo $isofeaturedimage; ?>" alt="" />
+        <div class="w-4/6 pl-5">
+            <span class="rounded-full bg-gray-200 px-3 py-1.5 text-xs font-medium text-gray-700 inline-block mb-2">
+            <?php echo implode(',', array_column($the_cat, 'cat_name')); ?>
+            </span>
+            <h2 class="mb-3 line-clamp-3 text-4xl font-bold leading-10"> <?php the_title() ?></h2>
+            <p class="line-clamp-3 leading-7">
+                <?php
+                echo wp_strip_all_tags( wp_trim_words( get_the_content(), 40, '...' ) );
+                ?>
+            </p>
+            <div class="mt-5 flex items-center">
+                <svg class="h-5 w-5 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path fill="currentColor" d="M6 1a1 1 0 0 0-2 0h2ZM4 4a1 1 0 0 0 2 0H4Zm7-3a1 1 0 1 0-2 0h2ZM9 4a1 1 0 1 0 2 0H9Zm7-3a1 1 0 1 0-2 0h2Zm-2 3a1 1 0 1 0 2 0h-2ZM1 6a1 1 0 0 0 0 2V6Zm18 2a1 1 0 1 0 0-2v2ZM5 11v-1H4v1h1Zm0 .01H4v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM10 11v-1H9v1h1Zm0 .01H9v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM10 15v-1H9v1h1Zm0 .01H9v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM15 15v-1h-1v1h1Zm0 .01h-1v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM15 11v-1h-1v1h1Zm0 .01h-1v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM5 15v-1H4v1h1Zm0 .01H4v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM2 4h16V2H2v2Zm16 0h2a2 2 0 0 0-2-2v2Zm0 0v14h2V4h-2Zm0 14v2a2 2 0 0 0 2-2h-2Zm0 0H2v2h16v-2ZM2 18H0a2 2 0 0 0 2 2v-2Zm0 0V4H0v14h2ZM2 4V2a2 2 0 0 0-2 2h2Zm2-3v3h2V1H4Zm5 0v3h2V1H9Zm5 0v3h2V1h-2ZM1 8h18V6H1v2Zm3 3v.01h2V11H4Zm1 1.01h.01v-2H5v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H5v2h.01v-2ZM9 11v.01h2V11H9Zm1 1.01h.01v-2H10v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H10v2h.01v-2ZM9 15v.01h2V15H9Zm1 1.01h.01v-2H10v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H10v2h.01v-2ZM14 15v.01h2V15h-2Zm1 1.01h.01v-2H15v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H15v2h.01v-2ZM14 11v.01h2V11h-2Zm1 1.01h.01v-2H15v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H15v2h.01v-2ZM4 15v.01h2V15H4Zm1 1.01h.01v-2H5v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H5v2h.01v-2Z" />
+                </svg>
+                <span class="ml-1 text-sm text-gray-400">May 2, 2022</span>
+            </div>
         </div>
-    </div>
-</article>
+    </article>
 
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('mb-12 hidden'); ?>>
